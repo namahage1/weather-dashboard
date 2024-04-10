9018f8bd9ef9dba4f9baadf5619dc997dce36f3b
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-04-09 23:47:28 -0600

    changed alignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,65 @@
-const cityName = "Oklahoma City";
+let cityName = "Castle Rock";//default just in case does not show up
+const inputCityEl = document.querySelector('#search-field');
 const apiKey = "657dd3403e0d0b938737ea9d758485c0";
-const coordinates = {
-    lat: 35.47,
-    lon: -97.51,
-};
 
 const requestOptions = {
     method: "GET",
     redirect: "follow"
   };
-  
 
+  const citySearchHandler = function (event) {
+    event.preventDefault();
+  
+    cityName =  document.getElementById('city_name').value;
+    if (cityName) {
+        console.log("city name is " + cityName);
+        getCoords(cityName);
+    } else {
+      alert('Please enter a city name');
+    }
+  };
+
+  function clearElements(id){
+    const parentDOM = document.getElementById(id);
+    if(parentDOM){
+      parentDOM.parentNode.removeChild(parentDOM);
+  }else
+  console.log("empty element");
+  }
+  
+  function getCurrentWeather(lat,lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+
+        console.log(result);
+        return result;
+    })
+    
+  }
 
 function getForcast(lat, lon){
   fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`, requestOptions)
     .then((response) => response.json())
     .then((result) => {
 
-
         console.log(result);
 
         const selectedData = [
-            result.list[0],
+          result.list[0],
             result.list[8],
             result.list[16],
             result.list[24],
             result.list[32],
         ]
+        clearElements("parentDOM");
 
-        console.log(selectedData)
-
-
-
+        const newCardDiv = document.createElement("div");//<div class="card">
+            newCardDiv.classList.add("card");
+            newCardDiv.setAttribute("style", "width: 18rem;")
+            newCardDiv.setAttribute("id", "parentDOM");
 
         for([i,s] of selectedData.entries()){
-            console.log(s.dt_txt);
-
-
             // <div class="card" style="width: 18rem;">
             //     <div class="card-header">
             //         04 08 2024
@@ -48,55 +71,61 @@ function getForcast(lat, lon){
             //         <li class="list-group-item">Wind Speed: </li>
             //     </ul>
             // </div>
-            const newCardDiv = document.createElement("div");
-            newCardDiv.classList.add("card");
-            newCardDiv.setAttribute("style", "width: 18rem;")
+            
 
-            const newDateDiv = document.createElement("div");
+            const newDateDiv = document.createElement("div"); //<div class="card-header"></div>
             newDateDiv.classList.add("card-header");
-            newDateDiv.textContent = s.dt_txt
+            newDateDiv.textContent = dayjs(s.dt_txt).format('MM/DD/YYYY'); //displays date
 
-            newCardDiv.append(newDateDiv);
+            newCardDiv.append(newDateDiv); //now  <div class="card" style="width: 18rem;">
+                                           //       <div class="card-header">
 
 
             // create iamge
             const newCardImg = document.createElement("img");
             newCardImg.setAttribute("src", "https://openweathermap.org/img/wn/"+s.weather[0].icon+"@2x.png")
 
-            newCardDiv.append(newCardImg);
+            newCardDiv.append(newCardImg); //<div class="card" style="width: 18rem;">
+                                           //          <img src=...>
 
             const newCardUl = document.createElement("ul");
-            newCardUl.classList = "list-group list-group-flush"
+            newCardUl.classList = "list-group list-group-flush";
             // newCardUl.classList.add("list-group")
             // newCardUl.classList.add("list-group-flush")
 
             const newCardTmpLi = document.createElement("li");
             newCardTmpLi.classList.add("list-group-item");
-            newCardTmpLi.textContent = "Temp: " + s.main.temp;
+            newCardTmpLi.textContent = "Temp: " + s.main.temp + " F";
 
             const newCardHumLi = document.createElement("li");
             newCardHumLi.classList.add("list-group-item");
-            newCardHumLi.textContent = s.main.humidity;
+            newCardHumLi.textContent = s.main.humidity + " %";
 
             const newCardWindLi = document.createElement("li");
             newCardWindLi.classList.add("list-group-item");
-            newCardWindLi.textContent = s.wind.speed;
+            newCardWindLi.textContent = s.wind.speed + " MPH";
 
-            newCardUl.append(newCardTmpLi)
+            newCardUl.append(newCardTmpLi) //<ul> <li>
             newCardUl.append(newCardHumLi)
             newCardUl.append(newCardWindLi)
 
-            newCardDiv.append(newCardUl)
+            newCardDiv.append(newCardUl)//<div class="card" style="width: 18rem;">
+                                        //   <ul><li>
 
-            document.getElementById("forecast-container").append(newCardDiv)
+                   // render the data into the page
+        
 
         }
-
-
-        // render the data into the page
         const myH1 = document.getElementById("searched-city")
-        myH1.textContent = result.city.name
-
+        myH1.textContent = result.city.name //display searched city on the element of <h1 id=searched-city>
+        const mainContainer = document.getElementById("forecast-container");
+        if(mainContainer){
+          mainContainer.append(newCardDiv);
+        }else{
+          console.log("forecast-container is null");
+        }
+        
+        let todayWeather = getCurrentWeather(lat,lon);
         // dayjs('2019-01-25').format('DD/MM/YYYY')
         // const todayDate = document.getElementById("today-date");
         // // todayDate.textContent = dayjs().add(1, "day").format('DD/MM/YYYY')
@@ -109,13 +138,14 @@ function getForcast(lat, lon){
 
 function getCoords(city = cityName){
     //get coordinate using city name   
-    console.log("meow");    
+    
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&limit=1&appid=${apiKey}`, requestOptions)
     .then((response) => response.json())
   .then((result) => getForcast(result.city.coord.lat,result.city.coord.lon))
     .catch((error) => console.error(error));
 }
-getCoords()
+getCoords()//render the first time
+inputCityEl.addEventListener('submit', citySearchHandler);
 //need to get city name from form to function
 //show the forcast on the screen
 //store in local storage
